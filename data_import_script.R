d9b9495fcfaf93b7628fc9dbb38bb00b56f9c88a
Dinesh23244 <dinesh23244@iiitd.ac.in>
2024-09-15 21:23:01 +0530

    Data Import from first source was done.

--- data_import_script.R
+++ data_import_script.R
@@ -1,2 +1,6 @@
 # set directory
   setwd("/home/dkdhiveen/Desktop/git-house/Test-repository-for-RStudio--dell_private-server-")
+  
+# Import data from source1
+  source1_data <- read.csv("~/Dowloads/SraRunTable.txt")
+  
